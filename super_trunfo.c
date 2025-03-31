cba02f9add1ef3fd4c4570c8262c9b32b3e70a84
Erick-victor-71 <erickvictor086@gmail.com>
2025-03-31 19:05:25 +0000

    Terminei o desafio

---
+++ super_trunfo.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(){
+    int populacao, npt; //npt=  numero de pontos turisiticos
+    char estado[8];
+    char cidade[30], codigo[10];
+    float pib, area;
+
+    printf("População:\n");
+    scanf("%d", &populacao);
+    
+    printf("Pontos turisiticos:\n");
+    scanf("%d", &npt);
+
+    printf("Cidade:\n");
+    scanf("%s", &cidade);
+
+    printf("Código:\n");
+    scanf("%s", &codigo);
+
+    printf("Pib:\n");
+    scanf("%f", &pib);
+
+    printf("Área:\n");
+    scanf("%f", &area); 
+
+    printf("Estado:\n");
+    scanf("%s", &estado);
+    printf("Seus dados:\n");
+    
+    
+
+    printf("População: %d\n", populacao);
+    printf("Pontos Turísticos: %d\n", npt);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade );
+    printf("Código: %s\n", codigo);
+    printf("Pib: %f\n", pib);
+    printf("Área: %f\n", area);
+
+    return 0;
+
+
+
+
+}
+
