02b5e5030d547f99f3a5b4e37c43399e4bceb95b
Erick-victor-71 <erickvictor086@gmail.com>
2025-03-31 20:15:22 +0000

    Desafio completo

--- super_trunfo.c
+++ super_trunfo.c
@@ -26,7 +26,7 @@ int main(){
 
     printf("Estado:\n");
     scanf("%s", &estado);
-    printf("Seus dados:\n");
+    printf("Carta1:\n");
     
     
 
@@ -37,6 +37,41 @@ int main(){
     printf("Código: %s\n", codigo);
     printf("Pib: %f\n", pib);
     printf("Área: %f\n", area);
+
+
+
+    printf("População:\n");
+    scanf("%d", &populacao);
+    
+    printf("Pontos turisiticos:\n");
+    scanf("%d", &npt);
+
+    printf("Cidade:\n");
+    scanf("%s", &cidade);
+
+    printf("Código:\n");
+    scanf("%s", &codigo);
+
+    printf("Pib:\n");
+    scanf("%f", &pib);
+
+    printf("Área:\n");
+    scanf("%f", &area); 
+
+    printf("Estado:\n");
+    scanf("%s", &estado);
+    printf("Carta 2:\n");
+    
+    
+
+    printf("População: %d\n", populacao);
+    printf("Pontos Turísticos: %d\n", npt);
+    printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade );
+    printf("Código: %s\n", codigo);
+    printf("Pib: %f\n", pib);
+    printf("Área: %f\n", area);
+
 
     return 0;
 
